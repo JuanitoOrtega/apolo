0578f28a3c520465a5f6dec8beba367926fd2c27
Juanito Ortega <ortegaj83@gmail.com>
2022-10-08 12:19:36 -0400

    app app created

---
+++ core/api/serializers.py
@@ -0,0 +1,10 @@
+from dataclasses import fields
+from rest_framework import serializers
+
+from core.pos.models import Category
+
+
+class CategorySerializers(serializers.ModelSerializer):
+  class Meta:
+    model = Category
+    fields = '__all__'
