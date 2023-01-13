8ff1f14d34e1872d075db1aaf3a7c7171360457c
Juanito Ortega <ortegaj83@gmail.com>
2023-01-13 05:09:28 +0000

    mejorando el deploy

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['5.78.43.135']
 
 
 # Application definition
@@ -74,7 +74,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
-        # 'PASSWORD': config('DB_PASS'),
+        'PASSWORD': config('DB_PASS'),
         'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', default=5432, cast=int),
     }
