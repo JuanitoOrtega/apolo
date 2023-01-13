c6d929c6b3bd5b3e38bff93d49b6a8db696914b2
Juanito Ortega <ortegaj83@gmail.com>
2023-01-13 02:48:07 +0000

    manage.py updated

--- deploy/server.sh
+++ deploy/server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DJANGODIR=$(dirname $(cd `dirname $0` && pwd))
-DJANGO_SETTINGS_MODULE=config.settings
+DJANGO_SETTINGS_MODULE=config.production
 cd $DJANGODIR
 source env/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
